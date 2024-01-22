915d48efdf245879ac901c7b3f1aad1392d62f7c
vzffvr <157378831+vzffvr@users.noreply.github.com>
2024-01-22 15:37:11 +0100

    Ajt workspace

--- SAE_BUT2_22janv2024/src/main.cpp
+++ SAE_BUT2_22janv2024/src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <wire.h>
+#include <Ticker.h>
 
 // constantes
 const uint8_t LED_PIN = 13;
@@ -7,27 +9,46 @@ const uint8_t LED_PIN = 13;
 void action();
 
 // objets
-
+Ticker ticker(action, 500, 0, MILLIS);
 // variables globales
 //*****
 void action()
 {
     Serial.println("bonjour");
+    uint8_t st = digitalRead(LED_PIN);
     delay(500);
+    if (st)
+    {
+        digitalWrite(LED_PIN, 0);
+    }
+    else
+    {
+        digitalWrite(LED_PIN, 1);
+    }
 }
 //*****
 
 void setup()
 {
+    ticker.start();
     Serial.begin(115200);
     pinMode(LED_PIN, OUTPUT);
     digitalWrite(LED_PIN, LOW);
+    /* bool res = Wire.begin();
+    if (res == false)
+    {
+    }
+ */
 }
 
 void loop()
 {
-    action();
-    digitalWrite(LED_PIN, HIGH);
-    action();
-    digitalWrite(LED_PIN, LOW);
+    /* Wire.beginTransmission(0x45);
+    Wire.write(3);
+    Wire.endTransmission();
+    Wire.requestFrom(0x45, byte(1));
+    uint8_t val = Wire.read();
+    Wire.endTransmission(); */
+
+    ticker.update();
 }
